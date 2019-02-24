91409bcbfa0e0677870ee396f847821e24469e45
Sofia Lunkes <sofia.big@live.com>
2019-02-23 23:40:03 -0300

    bonus solution

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,7 +8,7 @@ proper order even if all the requests haven't finished.
 // Inline configuration for jshint below. Prevents `gulp jshint` from failing with quiz starter code.
 /* jshint unused: false */
 
-(function(document) {
+(function (document) {
   'use strict';
 
   var home = null;
@@ -26,11 +26,15 @@ proper order even if all the requests haven't finished.
    * @param  {Object} data - The raw data describing the planet.
    */
   function createPlanetThumb(data) {
-    var pT = document.createElement('planet-thumb');
-    for (var d in data) {
-      pT[d] = data[d];
-    }
-    home.appendChild(pT);
+    return new Promise(function (resolve) {
+      var pT = document.createElement('planet-thumb');
+      for (var d in data) {
+        pT[d] = data[d];
+      }
+      home.appendChild(pT);
+      console.log('rendered:' + data.pl_name);
+      resolve();
+    });
   }
 
   /**
@@ -48,16 +52,40 @@ proper order even if all the requests haven't finished.
    * @return {Promise}    - A promise that passes the parsed JSON response.
    */
   function getJSON(url) {
-    return get(url).then(function(response) {
-      return response.json();
+    console.log('sent: ' + url);
+    return get(url).then(function (response) {
+      if (url === 'data/planets/Kepler-62f.json') {
+        return new Promise(function (resolve) {
+          setTimeout(function () {
+            console.log('received: ' + url);
+            resolve(response.json());
+          }, 500);
+        });
+      } else {
+        console.log('received: ' + url);
+        return response.json();
+      }
     });
   }
 
-  window.addEventListener('WebComponentsReady', function() {
+  window.addEventListener('WebComponentsReady', function () {
     home = document.querySelector('section[data-route="home"]');
-    /*
-    Your code goes here!
-     */
-    // getJSON('../data/earth-like-results.json')
+    getJSON('../data/earth-like-results.json')
+      .then(function (response) {
+        addSearchHeader(response.query);
+        return response;
+      })
+      .then(function (response) {
+        var sequence = Promise.resolve();
+
+        var arrayOfExecutingPromises = response.results.map(function (result) {
+          return getJSON(result);
+        });
+        arrayOfExecutingPromises.forEach(function (request) {
+          sequence = sequence.then(function () {
+            return request.then(createPlanetThumb);
+          });
+        });
+      });
   });
 })(document);
